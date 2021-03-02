7477418d9da2fcfa0a3f4c31a86fa913f1c2356c
aymanesarrar <aymensarrar1@gmail.com>
2021-03-02 01:21:32 +0100

    my first python code

---
+++ README.md
@@ -0,0 +1,3 @@
+# baby-python-steps
+# this is my first time trying python
+idk man fuck python wtf is this shit
---
+++ hello.py
@@ -0,0 +1,18 @@
+# this program says hello and asks for my name :))
+print('Hello world')
+# asks for his name
+print('what is your name ?')
+# writing the string to the myName variable
+# fuck python but it's fun
+myName = input()
+# fuck python again this looks like javascript
+print('it is good to meet you ' + myName)
+# calculating the length of the name with len func and casting it from int to string using str function
+print('the length of your name is ' + str(len(myName)))
+# asking for his age
+print('what is your age ?')
+# putting the age  on myAge variable and fuck python
+myAge = input()
+# yeah yeah he will be fucking age in 1 year yeah yeah fuck this
+# the int function it convert the myAge to int and then convert it to string and yeah fuck python
+print('you will be ' + str(int(myAge) + 1) + ' in a year')
