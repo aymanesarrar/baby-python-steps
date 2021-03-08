dcba4462e6fbe02b71438c0e1dd881a3d652958e
aymanesarrar <aymensarrar1@gmail.com>
2021-03-09 00:22:24 +0100

    remaking the relace program into a function yey

--- replace.py
+++ replace.py
@@ -1,9 +1,13 @@
-print('enter a paragraph');
+print('enter a paragraph')
 paragraph = input()
 letter_one = input()
 letter_two = input()
-paragraph = list(paragraph)
-for i in range(len(paragraph)):
-  if (paragraph[i] == letter_one):
-    paragraph[i] = letter_two
-print(''.join(paragraph))
+def _replace(paragraph, letter_one, letter_two):
+  paragraph = list(paragraph)
+  for i in range(len(paragraph)):
+    if (paragraph[i] == letter_one):
+      paragraph[i] = letter_two
+  return ''.join(paragraph)
+
+str = _replace(paragraph,letter_one,letter_two)
+print(str)
