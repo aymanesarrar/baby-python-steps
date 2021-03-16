009cc8c963206444940c3146f69cf7d678e277ea
aymanesarrar <aymensarrar1@gmail.com>
2021-03-16 19:09:32 +0100

    copying and moving files/folders

---
+++ more_files.py
@@ -0,0 +1,6 @@
+import shutil
+#shutil.copy() copy a file into a folder
+#shutil.copytree() copy a directory
+#shutil.move() move a file to a new location
+# you can also rename a file using shutil.move()
+shutil.copy('test.txt', '/mnt/c/Users/hp/Desktop/react-projects/REACT/crash')
